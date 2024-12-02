a629b95ced90ddc3ce0354993d18cd2837c68825
Max Petersen <maxvonpetersen@gmail.com>
2024-12-02 15:17:46 -0700

    finished day 2 part 2, kind of janky though might fix

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -14,57 +14,42 @@ fn check_diff(prev: isize, current: isize, total_diff: isize) -> bool {
         || (total_diff.signum() != 0 && diff.signum() != total_diff.signum()));
 }
 
-fn is_safe(report: Vec<isize>) -> bool {
+fn is_safe(report: &Vec<isize>) -> bool {
     let mut prev: Option<isize> = None;
     let mut total_diff: isize = 0;
     for n in report {
         if let Some(m) = prev {
-            if !check_diff(m, n, total_diff) {
+            if !check_diff(m, *n, total_diff) {
                 return false;
             }
-            prev = Some(n);
+            prev = Some(*n);
             total_diff += n - m;
         } else {
-            prev = Some(n);
+            prev = Some(*n);
         }
     }
     return true;
 }
 
-fn is_safe_dampener(report: Vec<isize>) -> bool {
-    let mut prev: Option<isize> = None;
-    let mut total_diff: isize = 0;
-    let mut idx = 0;
-    for n in &report {
-        if let Some(m) = prev {
-            let diff = n - m;
-            if !check_diff(m, *n, total_diff) {
-                let mut prev_removed = report.clone();
-                prev_removed.remove(idx - 1);
-                if is_safe(prev_removed) {
-                    return true;
-                }
-                let mut curr_removed = report.clone();
-                curr_removed.remove(idx);
-                if is_safe(curr_removed) {
-                    return true;
-                }
-                return false;
-            }
-            prev = Some(*n);
-            total_diff += diff;
-        } else {
-            prev = Some(*n);
-        }
-        idx += 1;
+fn is_safe_dampener(report: &Vec<isize>) -> bool {
+    if is_safe(report) {
+        return true;
     }
-    return true;
+    for n in 0..report.len() {
+        let mut mod_report = report.clone();
+        mod_report.remove(n);
+        if is_safe(&mod_report) {
+            return true;
+        }
+    }
+    return false;
 }
 
 fn part_1(data: &str) {
     let mut safe = 0;
     for report in data.lines() {
-        if is_safe(parse_report(report)) {
+        let parsed_report = parse_report(report);
+        if is_safe(&parsed_report) {
             safe += 1
         }
     }
@@ -73,10 +58,13 @@ fn part_1(data: &str) {
 
 fn part_2(data: &str) {
     let mut safe = 0;
+    let mut idx = 0;
     for report in data.lines() {
-        if is_safe_dampener(parse_report(report)) {
+        let parsed_report = parse_report(report);
+        if is_safe_dampener(&parsed_report) {
             safe += 1
         }
+        idx += 1;
     }
     println!("Safe reports: {safe}");
 }
