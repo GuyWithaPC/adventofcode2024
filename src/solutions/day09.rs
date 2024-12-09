3096024ea3c999ef5df6b536815724ab5601903c
Max Petersen <maxvonpetersen@gmail.com>
2024-12-08 23:01:20 -0700

    finished day 9 part 2

--- src/solutions/day09.rs
+++ src/solutions/day09.rs
@@ -1,7 +1,8 @@
 use itertools::{repeat_n, Itertools};
 
 crate::day!("Disk Fragmenter" => {
-    part_1
+    part_1,
+    part_2
 });
 
 fn part_1(data: &str) -> usize {
@@ -39,11 +40,77 @@ fn part_1(data: &str) -> usize {
     reconstructed.iter().enumerate().map(|(i, &x)| i * x).sum()
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Block {
+    Full(usize, usize),
+    Empty(usize)
+}
+
+fn part_2(data: &str) -> usize {
+    let mut blocks: Vec<Block> = data.chars().filter(|&c| c != '\n')
+        .batching(|it| {
+            if let Some(a) = it.next() {
+                if let Some(b) = it.next() {
+                    Some((a.to_digit(10).unwrap() as usize, b.to_digit(10).unwrap() as usize))
+                } else {
+                    Some((a.to_digit(10).unwrap() as usize, 0usize))
+                }
+            } else {
+                None
+            }
+        })
+        .enumerate()
+        .map(|(i, (full, empty))| {
+            vec![Block::Full(i, full), Block::Empty(empty)]
+        })
+        .flatten()
+        .collect();
+    let mut consolidate = blocks.clone();
+    blocks.reverse();
+    for block in blocks {
+        if let Block::Full(i, full_size) = block {
+            let (found, full_block, empty_block, index) = {
+                if let Some((index, Block::Empty(empty_size))) = consolidate.iter().enumerate()
+                    .take_while(|(_, &b)| match b {Block::Full(index, _) => index != i, Block::Empty(_) => true})
+                    .find(|(_, &block)| match block { Block::Full(_,_) => false, Block::Empty(size) => size >= full_size})
+                {
+                    (true, Block::Full(i, full_size), Block::Empty(*empty_size - full_size), index)
+                } else {
+                    (false, Block::Empty(0), Block::Empty(0), 0)
+                }
+            };
+            if found {
+                let (moved_index, moved_size) = consolidate.iter().enumerate()
+                    .find(|(_, &b)| b == block)
+                    .map(|(i, &b)| (i, match b {Block::Full(_, s) => s, Block::Empty(s) => s}))
+                    .unwrap();
+                consolidate.remove(moved_index);
+                consolidate.insert(moved_index, Block::Empty(moved_size));
+                consolidate.remove(index);
+                consolidate.insert(index, full_block);
+                consolidate.insert(index + 1, empty_block);
+            }
+        }
+    }
+    consolidate.iter()
+        .map(|&b| {
+            match b {
+                Block::Full(index, size) => repeat_n(index, size),
+                Block::Empty(size) => repeat_n(0, size)
+            }
+        })
+        .flatten()
+        .enumerate()
+        .map(|(i, x)| i * x)
+        .sum()
+}
+
 crate::test_day!(
 "
 2333133121414131402
 ",
 {
-    part_1 => 1928
+    part_1 => 1928,
+    part_2 => 2858
 }
 );
