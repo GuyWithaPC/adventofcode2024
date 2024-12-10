7d23de0ecd5c79af8094b3d9717057e0c612aca6
Max Petersen <maxvonpetersen@gmail.com>
2024-12-09 22:28:40 -0700

    finished day 10 part 1

--- src/solutions/day10.rs
+++ src/solutions/day10.rs
@@ -1,7 +1,82 @@
-crate::day!("" => {
+use std::collections::{HashMap, HashSet};
 
+use crate::util::positions::Vec2;
+
+crate::day!("Hoof It" => {
+    part_1
 });
 
-// TODO
+type Position = Vec2<isize>;
+type Grid = HashMap<Position, usize>;
 
-crate::test_day!("", {});
+fn part_1(data: &str) -> usize {
+    let map = parse_input(data);
+    let trailheads = get_number(&map, 0);
+    trailheads.iter().map(|&start| {
+        get_number(&filter_unreachable(&map, start), 9).len()
+    }).sum()
+}
+
+fn parse_input(input: &str) -> Grid {
+    input.lines().enumerate().flat_map(|(y, row)| {
+        row.chars().enumerate().map(move |(x, c)| {
+            (Position::new(x as isize, y as isize), c.to_digit(10).unwrap() as usize)
+        })
+    }).collect()
+}
+
+fn get_number(map: &Grid, num: usize) -> Vec<Position> {
+    map.iter().filter_map(|(&pos, &value)| {
+        if value == num {
+            Some(pos)
+        } else {
+            None
+        }
+    }).collect()
+}
+
+/// filter out unreachable spots in a copy of the given map
+fn filter_unreachable(map: &Grid, start: Position) -> Grid {
+    let directions: [Vec2<isize>;4] = [Vec2::up(), Vec2::right(), Vec2::down(), Vec2::left()];
+    let mut filtered: HashMap<Position, usize> = HashMap::new();
+    let mut to_visit: Vec<(usize, Position)> = Vec::new();
+    filtered.insert(start, map.get(&start).copied().unwrap());
+    for dir in directions {
+        to_visit.push((0, start + dir));
+    }
+    loop {
+        let check = to_visit.pop();
+        if check.is_none() {
+            break;
+        }
+        let (last, check) = check.unwrap();
+        if let Some(check_val) = map.get(&check).copied() {
+            if check_val != 0 && check_val - 1 == last {
+                filtered.insert(check, check_val);
+                for dir in directions {
+                    if filtered.contains_key(&(check + dir)) {
+                        continue;
+                    }
+                    to_visit.push((check_val, check + dir));
+                }
+            }
+        }
+    }
+    return filtered;
+}
+
+crate::test_day!(
+"
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+",
+{
+    part_1 => 36
+}
+);
