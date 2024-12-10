b18cb8c8e2ea572a33494d08e2ab8e7b90bee47e
Max Petersen <maxvonpetersen@gmail.com>
2024-12-09 22:41:12 -0700

    finished day 10

--- src/solutions/day10.rs
+++ src/solutions/day10.rs
@@ -1,9 +1,12 @@
 use std::collections::{HashMap, HashSet};
 
+use itertools::Itertools;
+
 use crate::util::positions::Vec2;
 
 crate::day!("Hoof It" => {
-    part_1
+    part_1,
+    part_2
 });
 
 type Position = Vec2<isize>;
@@ -14,6 +17,15 @@ fn part_1(data: &str) -> usize {
     let trailheads = get_number(&map, 0);
     trailheads.iter().map(|&start| {
         get_number(&filter_unreachable(&map, start), 9).len()
+    }).sum()
+}
+
+fn part_2(data: &str) -> usize {
+    let map = parse_input(data);
+    let starts = get_number(&map, 0);
+    let ends = get_number(&map, 9);
+    starts.iter().cartesian_product(ends).map(|(&start, end)| {
+        distinct_trails(&map, start, end)
     }).sum()
 }
 
@@ -65,6 +77,32 @@ fn filter_unreachable(map: &Grid, start: Position) -> Grid {
     return filtered;
 }
 
+fn distinct_trails(map: &Grid, start: Position, end: Position) -> usize {
+    let directions: [Vec2<isize>;4] = [Vec2::up(), Vec2::right(), Vec2::down(), Vec2::left()];
+    let mut to_visit: Vec<(usize, Position)> = Vec::new();
+    let mut paths = 0;
+    to_visit.push((0, start));
+    loop {
+        let check = to_visit.pop();
+        if check.is_none() {
+            break;
+        }
+        let (check_val, check) = check.unwrap();
+        if check == end {
+            paths += 1;
+            continue;
+        }
+        for dir in directions {
+            if let Some(v) = map.get(&(check + dir)).copied() {
+                if v == check_val + 1 {
+                    to_visit.push((v, check + dir));
+                }
+            }
+        }
+    }
+    return paths;
+}
+
 crate::test_day!(
 "
 89010123
@@ -77,6 +115,7 @@ crate::test_day!(
 10456732
 ",
 {
-    part_1 => 36
+    part_1 => 36,
+    part_2 => 81
 }
 );
