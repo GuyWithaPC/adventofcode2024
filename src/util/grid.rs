662fcc6b3f779bf4d28b40a4427fdfb977d2f791
Max Petersen <maxvonpetersen@gmail.com>
2024-12-10 20:11:26 -0700

    added some iterator collect methods

--- src/util/grid.rs
+++ src/util/grid.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 #[derive(Clone)]
 pub struct Grid<T> 
 where T: Clone {
@@ -129,13 +131,71 @@ where T: Clone {
         self.remove(x, y)
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = Option<&T>> {
-        self.data.iter().map(|v| v.as_ref())
+    pub fn iter_lines(&self) -> impl Iterator<Item = Option<&T>> {
+        self.data.iter().chunks(self.width).map(|v| v.as_ref())
     }
 
-    pub fn iter_with_coords<C: From<(usize, usize)>>(&self) -> impl Iterator<Item = (C, Option<&T>)> {
+    pub fn iter_coords<C: From<(usize, usize)>>(&self) -> impl Iterator<Item = (C, Option<&T>)> {
         self.data.iter().enumerate().map(|(i, v)| {
             ((i % self.width, i / self.width).into(), v.as_ref())
         })
+    }
+}
+
+impl <T> Grid<Option<T>>
+where T: Clone {
+    pub fn unwrap_options(self) -> Grid<T> {
+        Grid {
+            data: self.data.into_iter().map(|o| match o {
+                Some(v) => v,
+                None => None
+            }).collect(),
+            width: self.width,
+            height: self.height
+        }
+    }
+}
+
+impl <T, C> FromIterator<(C, Option<T>)> for Grid<T>
+where C: Into<(usize, usize)>, T: Clone {
+    fn from_iter<IT: IntoIterator<Item = (C, Option<T>)>>(iter: IT) -> Self {
+        let mut new_values: Vec<(usize, usize, T)> = Vec::new();
+        let (width, height) = iter.into_iter().fold((0usize, 0usize), |(width, height), (coord, val)| {
+            let (x, y) = coord.into();
+            if let Some(v) = val {
+                new_values.push((x, y, v));
+            }
+            let new_width = std::cmp::max(x, width);
+            let new_height = std::cmp::max(y, height);
+            (new_width, new_height)
+        });
+        let mut grid = Grid::new(width, height);
+        for (x, y, v) in new_values {
+            grid.set(x, y, v);
+        }
+        grid
+    }
+}
+
+impl <T> FromIterator<Vec<T>> for Grid<T>
+where T: Clone {
+    fn from_iter<IT: IntoIterator<Item = Vec<T>>>(iter: IT) -> Self {
+        let mut grid_storage: Vec<Option<T>> = Vec::new();
+        let (width, height) = iter.into_iter()
+            .map(|i| {
+                (i.len(), i.into_iter().map(|v| Some(v)).collect())
+            })
+            .fold((0usize, 0usize), |(width, height), (row_width, mut row)| {
+                if width != 0 && row_width != width {
+                    panic!("Attempted to make a Grid from an iterator with variable row widths");
+                }
+                grid_storage.append(&mut row);
+                (row_width, height + 1)
+            });
+        Self {
+            data: grid_storage,
+            width,
+            height
+        }
     }
 }
